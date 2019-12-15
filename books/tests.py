a7f1f1923f19ee0411e161970cb455a853011fad
Wolfgang Giersche <wgiersche@gmail.com>
2019-12-15 08:10:05 +0100

    finished chapter 10: Books

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Book
+
+TITLE = "Some Title"
+AUTHOR = "An Author"
+PRICE = 23.00
+
+
+class BookTests(TestCase):
+
+    def setUp(self):
+        self.book = Book.objects.create(
+            title=TITLE,
+            author=AUTHOR,
+            price=PRICE
+        )
+
+    def test_book_list_view(self):
+        response = self.client.get(reverse('book_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, TITLE)
+        self.assertTemplateUsed(response, 'books/book_list.html')
+
+    def test_book_detail_view(self):
+        response = self.client.get(self.book.get_absolute_url())
+        no_response = self.client.get('/books/12345')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, AUTHOR)
+        self.assertTemplateUsed(response, 'books/book_detail.html')
--- scripts/verify_sendgrid.py
+++ scripts/verify_sendgrid.py
@@ -1,29 +1,25 @@
-import unittest
-
 import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 
-class VerifySendgrid:
+def test_post_via_sendgrid():
+    message = Mail(
+        from_email='wgiersche@gmail.com',
+        to_emails='wgiersche@gmail.com',
+        subject='Sending with Twilio SendGrid is Fun',
+        html_content='<strong>and easy to do anywhere, even with Python</strong>')
 
-    def test_post_via_sendgrid(self):
-        message = Mail(
-            from_email='wgiersche@gmail.com',
-            to_emails='wgiersche@gmail.com',
-            subject='Sending with Twilio SendGrid is Fun',
-            html_content='<strong>and easy to do anywhere, even with Python</strong>')
-
-        try:
-            apikey = os.environ.get('SENDGRID_API_KEY')
-            sg = SendGridAPIClient(apikey)
-            response = sg.send(message)
-            print(response.status_code)
-            print(response.body)
-            print(response.headers)
-        except Exception as e:
-            print(str(e))
+    try:
+        apikey = os.environ.get('SENDGRID_API_KEY')
+        sg = SendGridAPIClient(apikey)
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(str(e))
 
 
 if __name__ == '__main__':
-    VerifySendgrid().test_post_via_sendgrid()
+    test_post_via_sendgrid()
